de3b0247590a9f2b40c5f2c44f7098fb5d28216c
alexthegreat194 <alexander.gentry@aol.com>
2021-11-05 15:35:40 -0700

    added some functions

--- hero.py
+++ hero.py
@@ -1,14 +1,22 @@
 import random
+from ability import Ability
+from armor import Armor
 
 class Hero:
-  # We want our hero to have a default "starting_health",
-  # so we can set that in the function header.
+    # We want our hero to have a default "starting_health",
+    # so we can set that in the function header.
     def __init__(self, name, starting_health=100):
         '''Instance properties:
-        name: String
-        starting_health: Integer
-        current_health: Integer
+            abilities: List
+            armors: List
+            name: String
+            starting_health: Integer
+            current_health: Integer
         '''
+        # abilities and armors don't have starting values,
+        # and are set to empty lists on initialization
+        self.abilities = list()
+        self.armors = list()
         # we know the name of our hero, so we assign it here
         self.name = name
         # similarly, our starting health is passed in, just like name
@@ -17,17 +25,79 @@ class Hero:
         # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
 
-    def fight(self, opponent):
-        ''' Current Hero will take turns fighting the opponent hero passed in.'''
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+
+        # We use the append method to add ability objects to our list.
+        self.abilities.append(ability)
+
+    def attack(self):
+        '''Calculate the total damage from all ability attacks.
+            return: total_damage:Int
+        '''
+
+        # start our total out at 0
+        total_damage = 0
+        # loop through all of our hero's abilities
+        for ability in self.abilities:
+            # add the damage of each attack to our running total
+            total_damage += ability.attack()
+        # return the total damage
+        return total_damage
+
+    def add_armor(self, armor):
+        '''Add armor to self.armors
+            Armor: Armor Object
+        '''
+        # TODO: Add armor object that is passed in to `self.armors`
+        self.armors.append(armor)
+
+    def defend(self):
+        '''Calculate the total block amount from all armor blocks.
+            return: total_block:Int
+        '''
+        # TODO: This method should run the block method on each armor in self.armors
+        block_amt = 0
+        for armor in self.armors:
+            block_amt += armor.block()
+        return block_amt
+
+    def take_damage(self, damage):
+        '''Updates self.current_health to reflect the damage minus the defense.
+        '''
+        # TODO: Create a method that updates self.current_health to the current
+        # minus the the amount returned from calling self.defend(damage).
+        damage -= self.defend()
+        if damage < 0:
+            damage = 0
+        self.current_health -= 0
+
+    def is_alive(self):  
+        '''Return True or False depending on whether the hero is alive or not.
+        '''
+        # TODO: Check the current_health of the hero.
+        # if it is <= 0, then return False. Otherwise, they still have health
+        # and are therefore alive, so return True
+        return self.current_health <= 0
+
+    def fight(self, opponent):  
+        ''' Current Hero will take turns fighting the opponent hero passed in.
+        '''
         # TODO: Fight each hero until a victor emerges.
         # Phases to implement:
-        #1) randomly choose winner,
-        # Hint: Look into random library, more specifically the choice method  
-        hero_win = random.randint(0,1)
-        if hero_win:
-            print(f"{self.name} WINS!!!!")
-        else:
-            print(f"{opponent.name} WINS!!!!")
+        # 0) check if at least one hero has abilities. If no hero has abilities, print "Draw"
+        if len(self.abilities) <= 0 and len(opponent.abilities) <= 0:
+            print("Draw")
+            return
+        # 1) else, start the fighting loop until a hero has won
+        fighting = True
+        while fighting:
+        # 2) the hero (self) and their opponent must attack each other and each must take damage from the other's attack
+            
+        # 3) After each attack, check if either the hero (self) or the opponent is alive
+        
+        # 4) if one of them has died, print "HeroName won!" replacing HeroName with the name of the hero, and end the fight loop
+
 
 if __name__ == "__main__":
     # If you run this file from the terminal
@@ -39,3 +109,27 @@ if __name__ == "__main__":
     hero1 = Hero("Wonder Woman")
     hero2 = Hero("Dumbledore")
     hero1.fight(hero2)
+
+    ability = Ability("Great Debugging", 50)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    print(hero.abilities)
+
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
+
+    hero = Hero("Grace Hopper", 200)
+    shield = Armor("Shield", 50)
+    hero.add_armor(shield)
+    hero.take_damage(50)
+    print(hero.current_health)
+
+    hero = Hero("Grace Hopper", 200)
+    hero.take_damage(150)
+    print(hero.is_alive())
+    hero.take_damage(15000)
+    print(hero.is_alive())
