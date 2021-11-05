4099c4416e247883d2df31880bc505956c0470c7
alexthegreat194 <alexander.gentry@aol.com>
2021-11-05 14:00:56 -0700

    added abilities and armor

--- hero.py
+++ hero.py
@@ -1,28 +1,41 @@
-# hero.py
+import random
+
 class Hero:
   # We want our hero to have a default "starting_health",
   # so we can set that in the function header.
-  def __init__(self, name, starting_health=100):
-    '''Instance properties:
-      name: String
-      starting_health: Integer
-      current_health: Integer
-    '''
-    self.name = name
-    self.starting_health = starting_health
-    self.current_health = starting_health
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+        name: String
+        starting_health: Integer
+        current_health: Integer
+        '''
+        # we know the name of our hero, so we assign it here
+        self.name = name
+        # similarly, our starting health is passed in, just like name
+        self.starting_health = starting_health
+        # when a hero is created, their current health is
+        # always the same as their starting health (no damage taken yet!)
+        self.current_health = starting_health
 
-    # we know the name of our hero, so we assign it here
-    self.name = name
-    # similarly, our starting health is passed in, just like name
-    self.starting_health = starting_health
-    # when a hero is created, their current health is
-    # always the same as their starting health (no damage taken yet!)
-    self.current_health = starting_health
+    def fight(self, opponent):
+        ''' Current Hero will take turns fighting the opponent hero passed in.'''
+        # TODO: Fight each hero until a victor emerges.
+        # Phases to implement:
+        #1) randomly choose winner,
+        # Hint: Look into random library, more specifically the choice method  
+        hero_win = random.randint(0,1)
+        if hero_win:
+            print(f"{self.name} WINS!!!!")
+        else:
+            print(f"{opponent.name} WINS!!!!")
 
 if __name__ == "__main__":
-  # If you run this file from the terminal
-  # this block is executed.
-  my_hero = Hero("Grace Hopper", 200)
-  print(my_hero.name)
-  print(my_hero.current_health)
+    # If you run this file from the terminal
+    # this block is executed.
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
+
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    hero1.fight(hero2)
